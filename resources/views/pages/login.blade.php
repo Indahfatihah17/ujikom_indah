650efc9e4f9a45f5dbd77ad849f595fe77218eff
ajdnbaswdn <ajdnbaswdn>
2023-09-13 10:41:26 +0700

    memanipulasi file

---
+++ resources/views/pages/login.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <input type="username" name="" id="username" placeholder="Username">
+    <br>
+    <input type="password" name="" id="password" placeholder="Password">
+    <br>
+    <input type="button" value="Login">
+
+</body>
+</html>
